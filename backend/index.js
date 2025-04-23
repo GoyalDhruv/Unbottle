8497eef604a07b113666ee109579596db438a894
Dhruv <dhruvgoyal014@gmail.com>
2025-04-23 15:27:51 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import http from 'http';
+import { Server as SocketServer } from 'socket.io';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './config/db.js';
+// import userRoutes from './routes/userRoutes.js';
+// import chatRoutes from './routes/chatRoutes.js';
+// import socketHandler from './socket/index.js';
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+// const io = new SocketServer(server, {
+//     cors: { origin: '*' }
+// });
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+// app.use('/api/user', userRoutes);
+// app.use('/api/chat', chatRoutes);
+
+// socketHandler(io);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
